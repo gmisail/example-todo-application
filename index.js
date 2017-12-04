fbfc94146675afbdc95cd4831d2b6c9487daccaf
Graham Misail <gmisail64@gmail.com>
2017-12-04 11:59:31 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+var todo = document.getElementById("todo-items")
+var button = document.getElementById("todo-button")
+var field = document.getElementById("todo-field")
+
+button.onclick = function()
+{
+  todo.innerHTML += "<p>" + field.value + "  <button>X</button</p>"
+}
